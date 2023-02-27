77ac91edd3efb5746e72697516579edfed5e5ce6
madinaza <madinaza@code.edu.az>
2023-02-28 03:05:30 +0400

    Full Responsive Website

---
+++ assets/js/script.js
@@ -0,0 +1,373 @@
+$(document).ready(function () {
+  $("#first").owlCarousel({
+    loop: true,
+    margin: 15,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  $("#mainsec").owlCarousel({
+    loop: true,
+    margin: 15,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      340: {
+        items: 2,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+      1050: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  $("#third").owlCarousel({
+    loop: true,
+    margin: 15,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      500: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $("#fourth").owlCarousel({
+    loop: true,
+    margin: 15,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      500: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  $("#five").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 3,
+        nav: false,
+      },
+      500: {
+        items: 3,
+        nav: false,
+      },
+      900: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 6,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  $("#productdet").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      500: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $("#related").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      330: {
+        items: 2,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 5,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $("#faqBox").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 2,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $("#about").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 1,
+        nav: false,
+      },
+      700: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $("#about2").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 1,
+        nav: false,
+      },
+      700: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  $("#about3").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 1,
+        nav: false,
+      },
+      700: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+
+  $("#aboutlast").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      400: {
+        items: 2,
+        nav: false,
+      },
+      700: {
+        items: 2,
+        nav: false,
+      },
+      900: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+      1200: {
+        items: 5,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+});
